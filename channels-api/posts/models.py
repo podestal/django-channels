bbc09eb75742872cbe2ea954b0eedba0103a0ad1
podestal <luisrodriguezpodesta@gmail.com>
2024-08-13 13:14:57 -0400

    models done

--- channels-api/posts/models.py
+++ channels-api/posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    likes = models.ManyToManyField(User, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+
+    def __str__(self):
+        return str(self.title)
+    
+class Statistic(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(blank=True)
+
+    @property
+    def data(self):
+        return self.dataitem_set.all()
+
+    def __str__(self):
+        return str(self.name)
+
+class DataItem(models.Model):
+    statistic = models.ForeignKey(Statistic, on_delete=models.CASCADE)
+    value = models.PositiveIntegerField
