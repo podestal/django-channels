de1b72d9ec00dfd3c66eb03972be5092418d1d64
podestal <luisrodriguezpodesta@gmail.com>
2024-08-13 13:42:37 -0400

    consumers setup

--- channels-api/posts/consumers.py
+++ channels-api/posts/consumers.py
@@ -1,12 +1,23 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 class DashboardConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        pass
+        print('Connection')
+        await self.accept
 
     async def disconnect(self, close_code):
-        pass
+        print(f'Connection closed with code: {close_code}')
 
     async def receive(self, text_data):
-        pass
+        text_data_json = json.loads(text_data)
+        message = text_data_json('message')
+        sender = text_data_json('sender')
+
+        print(message, sender)
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'sender': sender,
+        }))
